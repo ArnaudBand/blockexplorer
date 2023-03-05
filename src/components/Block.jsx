3756dab5b5927bbcac3d872a723e4507e8fd1af7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-05 22:48:32 +0300

    Display the details for the future

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -12,31 +12,44 @@ const Block = () => {
   const [blockNumber, setBlockNumber] = useState();
   const [block, setBlock] = useState();
   const [transactions, setTransactions] = useState([]);
+  const [txs, setTxs] = useState();
 
   useEffect(() => {
     async function getBlockData() {
 
-        // get the latest block number
-        const latestBlockNumber = await alchemy.core.getBlockNumber();
-        setBlockNumber(latestBlockNumber);
+      // get the latest block number
+      const latestBlockNumber = await alchemy.core.getBlockNumber();
+      setBlockNumber(latestBlockNumber);
 
-        // get the latest block object
-        const latestBlock = await alchemy.core.getBlock(latestBlockNumber);
-        setBlock(latestBlock);
+      // get the latest block object
+      const latestBlock = await alchemy.core.getBlock(latestBlockNumber);
+      setBlock(latestBlock);
 
-        // get the list of transaction hashes in the block
-        const transactionHashes = latestBlock.transactions;
+      // get the list of transaction hashes in the block
+      const transactionHashes = latestBlock.transactions;
 
-        // get the transaction objects for each hash
-        const transactionObjects = await Promise.all(transactionHashes.map(hash => alchemy.core.getTransaction(hash)));
-        setTransactions(transactionObjects);
+      // get the transaction objects for each hash
+      const transactionObjects = await Promise.all(transactionHashes.map(hash => alchemy.core.getTransaction(hash)));
+      setTransactions(transactionObjects);
+
+      // Extract the data you need from the block and transactions
+      const blockData = {
+        blockNumber: latestBlock.number,
+        timestamp: latestBlock.timestamp,
+        transactions: transactionObjects.map(tx => ({
+          from: tx.from,
+          to: tx.to,
+        })),
+      };
+      setTxs(blockData);
     }
 
     getBlockData();
   }, []);
-
+  // console.log('Block:', block.hash)
   return (
     <div className="bg-gray-100 p-6">
+      {/* <p>Txs: {JSON.stringify(txs)}</p> */}
       <h2 className="text-3xl font-bold mb-4">Latest Block Information</h2>
       <div className="bg-white rounded-lg shadow p-4 mb-4">
         <p className="text-gray-500">Block Number:</p>
@@ -50,7 +63,7 @@ const Block = () => {
         <p className="text-gray-500">Transactions:</p>
         {transactions.length > 0 ? (
           <ul className="max-h-64 overflow-y-auto">
-            {transactions.map((transaction, index) => (
+            {transactions.slice(0, 10).map((transaction, index) => (
               <li key={index} className="mb-2">
                 <pre>{JSON.stringify(transaction, null, 2)}</pre>
               </li>
